05e18411e59b5cf245d9979f4f32c257bd98134c
SamueleTondelli <samueletondelli92@gmail.com>
2024-06-28 16:01:22 +0200

    removed unused form

--- cinema/movies/forms.py
+++ cinema/movies/forms.py
@@ -16,16 +16,3 @@ class SearchForm(forms.Form):
     sort_by_score = forms.BooleanField(label="Sort by score", required=False)
     
     
-class ReservationForm(forms.Form):
-    helper = FormHelper()
-    helper.form_id = "create_reservation_crispy_form"
-    helper.form_method = "POST"
-    helper.add_input(Submit("submit","Make Reservation"))
-    
-    def __init__(self, screening_id, *args, **kwargs):
-        super().__init__(args, kwargs)
-        s = get_object_or_404(MovieScreening, pk=screening_id)
-    
-    class Meta:
-        model = Reservation
-        fields = ["seats"]
