147a517c3c35eaec385c5e0a75f5ddfa0c649af9
SamueleTondelli <samueletondelli92@gmail.com>
2024-07-11 17:54:46 +0200

    added MovieScreening tests

--- cinema/movies/models.py
+++ cinema/movies/models.py
@@ -8,7 +8,7 @@ from math import sqrt
 
 class Tag(models.Model):
     name = models.CharField(max_length=15, unique=True)
-    
+
     def __str__(self):
         return self.name
 
@@ -20,7 +20,7 @@ class Movie(models.Model):
     duration = models.DurationField()
     tags = models.ManyToManyField(Tag, related_name="movies")
     cover = models.ImageField(upload_to="movies/covers", default="movies/covers/default.webp")
-    
+
     def get_score(self):
         revs = self.reviews.all()
         n = len(revs)
@@ -33,14 +33,14 @@ class Movie(models.Model):
     def can_user_review(self, user_id):
         if len(Review.objects.filter(user__id=user_id, movie=self)) > 0:
             return False
-        
+
         ress = Reservation.objects.filter(screening__movie=self, user__id=user_id).order_by("-screening__date")
         if len(ress) == 0:
             return False
-        
+
         if ress[0].screening.date > timezone.now():
             return False
-        
+
         return True
 
     def get_upcoming_movies():
@@ -50,46 +50,51 @@ class Movie(models.Model):
             if s.movie not in movies:
                 movies.append(s.movie)
         return movies
-    
+
     def __str__(self):
         return '"' + self.title + '" by "' + self.director + '"'
-    
+
 
 class Review(models.Model):
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name="reviews")
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="reviews")
     score = models.PositiveIntegerField(validators=[MaxValueValidator(10), MinValueValidator(1)])
     text = models.CharField(default="", max_length=300)
-    
+
     def __str__(self):
         return self.user.username + " gave " + self.movie.title + " a " + str(self.score) + "/10"
-    
+
     class Meta:
         unique_together = (('movie', 'user'))
-    
+
 
 
 class CinemaRoom(models.Model):
     name = models.CharField(max_length=10, unique=True)
-    seat_rows = models.IntegerField(default=10)
-    seat_cols = models.IntegerField(default=20)
-    
+    seat_rows = models.IntegerField(default=10, validators=[
+        MinValueValidator(1),
+        MaxValueValidator(26),
+    ])
+    seat_cols = models.IntegerField(default=20, validators=[
+        MinValueValidator(1)
+    ])
+
     def __str__(self):
         return self.name + " with " + str(self.seat_rows) + "x" + str(self.seat_cols) + " seats"
-    
+
     def get_total_seats(self):
         return self.seat_cols * self.seat_rows
-    
-    
+
+
 class MovieScreening(models.Model):
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name="screenings")
     room = models.ForeignKey(CinemaRoom, on_delete=models.CASCADE, related_name="screenings")
     seats = models.JSONField(default=dict)
     date = models.DateTimeField()
-    
+
     def clean(self):
         super().clean()
-        
+
         screenings = MovieScreening.objects.filter(room=self.room)
         for s in screenings:
             print(s.date - self.date)
@@ -99,8 +104,8 @@ class MovieScreening(models.Model):
             else:
                 if s.movie.duration >= self.date - s.date:
                     raise ValidationError(f"Error, screening overlapping with {s}")
-            
-    
+
+
     def init_seats(self):
         rows = self.room.seat_rows
         cols = self.room.seat_cols
@@ -109,34 +114,34 @@ class MovieScreening(models.Model):
         for r in range(0, rows):
             s[letters[r]] = [None] * cols
         self.seats = s
-        
+
     def get_seat_reserv(self, row, col, convert_col=False):
         if convert_col:
             col -= 1
-            
+
         if self.seats.get(row) == None or col < 0 or col >= self.room.seat_cols:
             raise Exception("Illegal argument in get_seat_reserv(), got row=" + str(row) + " col=" + str(col))
         return self.seats.get(row)[col]
-    
+
     def set_reservation(self, row, start_col, seats_number, res_id, convert_col=False):
         if convert_col:
             start_col -= 1
-        
+
         for c in range(start_col, start_col + seats_number):
             if self.get_seat_reserv(row, c, False) != None:
                 raise Exception(f"Tried reserving already occupied seats {row} {c}!")
-        
+
         r = self.seats.get(row)
         for c in range(start_col, start_col + seats_number):
             r[c] = res_id
         self.seats[row] = r
-        
+
     def remove_reservation(self, res_id):
         for r in self.seats:
             row = self.seats.get(r)
             row = [None if e == res_id else e for e in row]
             self.seats[r] = row
-        
+
     def get_free_seats(self):
         free = 0
         for r in self.seats:
@@ -150,19 +155,19 @@ class MovieScreening(models.Model):
 
     def __str__(self):
         return str(self.movie) + " on the " + str(self.date)
-    
+
 
 class Reservation(models.Model):
     screening = models.ForeignKey(MovieScreening, on_delete=models.CASCADE, related_name="reservations")
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="reservations")
     seats = models.JSONField(default=list)
-    
+
     def set_seats(self, row, start_col, seats_number):
         self.screening.set_reservation(row, start_col, seats_number, self.id)
         self.screening.save()
         for c in range(start_col, start_col + seats_number):
             self.seats.append((row, c))
-    
+
     def set_seat_from_idx(self, idx):
         letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
         row = letters[idx // self.screening.room.seat_cols]
@@ -170,17 +175,16 @@ class Reservation(models.Model):
         print(f"{idx}: {row}{col}")
         self.screening.set_reservation(row, col, 1, self.id)
         self.seats.append((row, col))
-    
+
     def get_seats(self):
         retval = ""
         l = sorted(self.seats)
         for s in l:
             retval += str(s[0]) + str(s[1] + 1) + ", "
         return retval[:-2]
-    
+
     def user_has_reviewed(self):
         return len(self.user.reviews.filter(movie=self.screening.movie)) > 0
-    
+
     def __str__(self):
         return str(self.screening) + " reserved by " + str(self.user)
-    
--- cinema/movies/tests.py
+++ cinema/movies/tests.py
@@ -1,3 +1,68 @@
 from django.test import TestCase
+from .models import *
 
-# Create your tests here.
+
+class MovieScreeningTests(TestCase):
+    def test_init_seats(self):
+        r = CinemaRoom(name="R", seat_rows=5, seat_cols=10)
+        s = MovieScreening()
+        s.room = r
+        s.init_seats()
+        seats = {
+            "A": [None] * 10,
+            "B": [None] * 10,
+            "C": [None] * 10,
+            "D": [None] * 10,
+            "E": [None] * 10,
+        }
+        self.assertEqual(s.seats, seats)
+
+    def test_set_reservation(self):
+        r = CinemaRoom(name="R", seat_rows=5, seat_cols=5)
+        s = MovieScreening()
+        s.room = r
+        s.init_seats()
+
+        s.set_reservation("A", 0, 2, 1)
+        seats = {
+            "A": [1, 1, None, None, None],
+            "B": [None] * 5,
+            "C": [None] * 5,
+            "D": [None] * 5,
+            "E": [None] * 5,
+        }
+        self.assertEqual(s.seats, seats)
+        self.assertRaises(Exception, s.set_reservation, "F", 0, 1, 2)
+        self.assertRaises(Exception, s.set_reservation, "A", 1, 2, 3)
+        self.assertRaises(Exception, s.set_reservation, "B", 2, 7, 4)
+        s.set_reservation("B", 3, 1, 5)
+        seats["B"] = [None, None, None, 5, None]
+        self.assertEqual(s.seats, seats)
+
+    def test_remove_reservation(self):
+        r = CinemaRoom(name="R", seat_rows=5, seat_cols=5)
+        s = MovieScreening()
+        s.room = r
+        s.init_seats()
+        s.set_reservation("A", 0, 2, 1)
+        s.set_reservation("B", 3, 1, 2)
+        s.remove_reservation(1)
+        seats = {
+            "A": [None] * 5,
+            "B": [None, None, None, 2, None],
+            "C": [None] * 5,
+            "D": [None] * 5,
+            "E": [None] * 5,
+        }
+        self.assertEqual(s.seats, seats)
+
+    def test_get_free_seats(self):
+        r = CinemaRoom(name="R", seat_rows=5, seat_cols=5)
+        s = MovieScreening()
+        s.room = r
+        s.init_seats()
+        self.assertEqual(s.get_free_seats(), 5*5)
+        s.set_reservation("A", 0, 2, 1)
+        self.assertEqual(s.get_free_seats(), 5*5 - 2)
+        s.set_reservation("B", 3, 1, 2)
+        self.assertEqual(s.get_free_seats(), 5*5 - 2 - 1)
