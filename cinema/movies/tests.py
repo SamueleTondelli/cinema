84b14fd227ab9816b65369d071a8a46170d3c530
SamueleTondelli <samueletondelli92@gmail.com>
2024-07-11 18:46:30 +0200

    added UpcomingMoviesView tests

--- cinema/movies/tests.py
+++ cinema/movies/tests.py
@@ -1,5 +1,7 @@
+from datetime import timedelta
 from django.test import TestCase
 from .models import *
+from django.urls import reverse
 
 
 class MovieScreeningTests(TestCase):
@@ -66,3 +68,53 @@ class MovieScreeningTests(TestCase):
         self.assertEqual(s.get_free_seats(), 5*5 - 2)
         s.set_reservation("B", 3, 1, 2)
         self.assertEqual(s.get_free_seats(), 5*5 - 2 - 1)
+
+
+class UpcomingMoviesViewTest(TestCase):
+    def test_no_upcoming_movies(self):
+        response = self.client.get(reverse("movies:upcomingmovies"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "There are no movies here")
+        self.assertQuerySetEqual(response.context["object_list"], [])
+
+    def test_upcoming_movies(self):
+        m1 = Movie(title="m1", duration=timedelta(minutes=120))
+        m2 = Movie(title="m2", duration=timedelta(minutes=120))
+        m3 = Movie(title="m3", duration=timedelta(minutes=120))
+        m4 = Movie(title="m4", duration=timedelta(minutes=120))
+        m1.save()
+        m2.save()
+        m3.save()
+        m4.save()
+
+        r = CinemaRoom(name="R", seat_rows=5, seat_cols=5)
+        r.save()
+
+        s1 = MovieScreening()
+        s1.movie = m1
+        s1.date = timezone.now() + timedelta(2)
+        s1.room = r
+        s1.save()
+
+        s2 = MovieScreening()
+        s2.movie = m2
+        s2.date = timezone.now() + timedelta(1)
+        s2.room = r
+        s2.save()
+
+        s3 = MovieScreening()
+        s3.movie = m3
+        s3.date = timezone.now() + timedelta(3)
+        s3.room = r
+        s3.save()
+
+        s4 = MovieScreening()
+        s4.movie = m4
+        s4.date = timezone.now() - timedelta(1)
+        s4.room = r
+        s4.save()
+
+        response = self.client.get(reverse("movies:upcomingmovies"))
+        self.assertEqual(response.status_code, 200)
+        self.assertNotContains(response, "There are no movies here")
+        self.assertQuerySetEqual(response.context["object_list"], [m2, m1, m3])
